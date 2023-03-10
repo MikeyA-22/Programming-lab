3d774bd6b452572db261b60a420d6faec282ff30
MikeyA-22 <117389570+MikeyA-22@users.noreply.github.com>
2023-03-10 05:27:52 -0500

    lab 6

---
+++ Assets/scripts/Collectable.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public string nameCollectable;
+    public int score;
+   
+
+    public Collectable(string name, int scorevalue)
+    {
+        this.nameCollectable = name;
+        this.score = scorevalue;
+        
+    }
+
+    public void UpdateScore()
+    {
+        ScoreManager.scoremanager.UpdateScore(score);
+    }
+
+  
+}
