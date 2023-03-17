4575b65324fdd8a2043b9188a5d6f772939b7a7e
MikeyA-22 <117389570+MikeyA-22@users.noreply.github.com>
2023-03-17 00:28:51 -0400

    lab 7 changes
    2 scripts added, character stats, health, and health ui added, as the loading for the coroutine

--- Assets/scripts/healthclass.cs
+++ Assets/scripts/healthclass.cs
@@ -1,24 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Characterstats))]
 
 public class healthclass : MonoBehaviour
 {
+    public GameObject uiPrefab;
+    public Transform target;
 
-    
-    
-        private int health_bar;
-
-
-        public void sethealth(int hel)
-        {
-            health_bar = hel;
-        }
-    
-
+    Transform ui;
+    Image healthSlider;
 
     private void Start()
     {
-            
+        ui = Instantiate(uiPrefab, target).transform;
+        ui.SetParent(target);
+        healthSlider = ui.GetChild(0).GetComponent<Image>();
+
+        GetComponent<Characterstats>().OnHealthChanged += OnHealthChanged;
+
+        // GetComponent<CharacterStats>().CurrentHealth = 120;
+        Debug.Log(GetComponent<Characterstats>().CurrentHealth);
+    }
+
+    void OnHealthChanged(int maxHealth, int currentHealth)
+    {
+        if (ui != null)
+        {
+            float healthPercent = (float)currentHealth / maxHealth;
+            healthSlider.fillAmount = healthPercent;
+
+            if (currentHealth <= 0)
+            {
+                Destroy(ui.gameObject);
+            }
+        }
     }
 }
